61b8313a0467ab4a64fff5eda99cced743f74f20
Marcos Adriano <marcosadriano740@gmail.com>
2025-04-29 20:44:27 -0300

    feat: add employee to departament of a company

--- common_collections/src/main.rs
+++ common_collections/src/main.rs
@@ -1,8 +1,33 @@
+use std::{collections::HashMap, io};
+
 mod median;
 mod mode;
 mod pig_latin;
 mod sorting;
 
 fn main() {
-    println!("Hello, world!");
+    let mut company: HashMap<String, Vec<String>> = HashMap::new();
+
+    println!("Add an employee to a departament!");
+    loop {
+        println!("====================");
+        println!("What do you want?");
+        println!("1 - Add an employee to a departament.");
+        println!("2 - List all employees in a departament.");
+        println!("3 - Exit.");
+        println!("====================");
+        let mut employee = String::new();
+        io::stdin()
+            .read_line(&mut employee)
+            .expect("Fail to read employee");
+
+        let mut departament = String::new();
+        io::stdin()
+            .read_line(&mut departament)
+            .expect("Fail to read departament");
+
+        let comp: &mut Vec<String> = company.entry(departament).or_insert(Vec::new());
+        comp.push(employee);
+        println!("{:?}", company);
+    }
 }
